02164a01a65f1223ae3ad27fb3ea0ad38bca5adb
Vittorioux <vittorioilmagno@gmail.com>
2025-04-04 16:59:39 -0300

    first commit

---
+++ sensor_system_statechart_Export.scm
@@ -0,0 +1,498 @@
+{
+  "graph": {
+    "cells": [
+      {
+        "position": {
+          "x": 0,
+          "y": 0
+        },
+        "size": {
+          "height": 10,
+          "width": 10
+        },
+        "angle": 0,
+        "type": "Statechart",
+        "id": "78749915-0da0-40a2-862f-9e8d94c7c68e",
+        "linkable": false,
+        "z": 1,
+        "attrs": {
+          "name": {
+            "text": "sensor_system_statechart Export"
+          },
+          "specification": {
+            "text": "@EventDriven\n@SuperSteps(no)\n\ninterface: \n    in event EV_SYS_XX_BTN_UP\n    in event EV_SYS_XX_BTN_DOWN\n\n"
+          }
+        }
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 205,
+          "y": 200
+        },
+        "size": {
+          "width": 100.82500457763672,
+          "height": 105
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+        "z": 9,
+        "embeds": [
+          "e1d91aba-8961-4528-a9a6-92817d64bde4"
+        ],
+        "attrs": {
+          "name": {
+            "text": "ST_SYS_XX_IDLE"
+          }
+        }
+      },
+      {
+        "type": "Entry",
+        "position": {
+          "x": 218,
+          "y": 138
+        },
+        "size": {
+          "height": 15,
+          "width": 15
+        },
+        "angle": 0,
+        "entryKind": "Initial",
+        "fixedRatio": true,
+        "embedable": false,
+        "linkable": true,
+        "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "z": 23,
+        "embeds": [
+          "f70f632c-e54d-4002-9ec5-7baec620de10"
+        ],
+        "attrs": {
+          "name": {
+            "fill": "#555555"
+          }
+        }
+      },
+      {
+        "type": "NodeLabel",
+        "label": true,
+        "size": {
+          "width": 15,
+          "height": 15
+        },
+        "position": {
+          "x": 218,
+          "y": 153
+        },
+        "id": "f70f632c-e54d-4002-9ec5-7baec620de10",
+        "z": 24,
+        "parent": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb",
+        "attrs": {
+          "label": {
+            "refX": "50%",
+            "textAnchor": "middle",
+            "refY": "50%",
+            "textVerticalAnchor": "middle"
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "a4d257fe-72cb-4334-9a1f-b5a8b4f886fb"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 51.00946044921875,
+              "dy": 4,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {},
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "9b5ce41b-0116-46a8-a603-ea4119c6e5d7",
+        "z": 25,
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 2.0076141357421875,
+              "dy": 94,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 3.0076141357421875,
+              "dy": 19,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_SYS_XX_BTN_DOWN"
+              }
+            },
+            "position": {
+              "offset": -76,
+              "angle": 0
+            }
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "2",
+                "fill": "#555555"
+              },
+              "circleBody": {
+                "fill": "#F0F0F0",
+                "stroke": "#555555"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "e1d91aba-8961-4528-a9a6-92817d64bde4",
+        "z": 27,
+        "parent": "36b8548a-74b1-4925-ae23-291c03231ee6",
+        "vertices": [
+          {
+            "x": 105,
+            "y": 264
+          }
+        ],
+        "attrs": {}
+      },
+      {
+        "type": "State",
+        "position": {
+          "x": 463,
+          "y": 200
+        },
+        "size": {
+          "width": 133,
+          "height": 105
+        },
+        "angle": 0,
+        "fixedRatio": false,
+        "embedable": true,
+        "linkable": true,
+        "id": "db372094-acc6-467b-b44d-495606208445",
+        "z": 30,
+        "embeds": [
+          "8e598810-1d2d-4cc1-8a22-68b4f8d3c937"
+        ],
+        "attrs": {
+          "name": {
+            "text": "ST_SYS_XX_ACTIVE"
+          }
+        }
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6"
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 5.007659912109375,
+              "dy": 22.346893310546875,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_SYS_XX_BTN_UP"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "042a4da3-fd99-425d-81ab-0e7953f00f73",
+        "z": 31,
+        "vertices": [],
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "db372094-acc6-467b-b44d-495606208445"
+        },
+        "target": {
+          "id": "36b8548a-74b1-4925-ae23-291c03231ee6",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 88.90945434570312,
+              "dy": 76,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_SYS_XX_BTN_DOWN"
+              }
+            },
+            "position": {}
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "1"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "17f83051-8d3c-4b74-8266-28012521ae31",
+        "z": 31,
+        "attrs": {}
+      },
+      {
+        "type": "Transition",
+        "source": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 127,
+              "dy": 93,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "target": {
+          "id": "db372094-acc6-467b-b44d-495606208445",
+          "anchor": {
+            "name": "topLeft",
+            "args": {
+              "dx": 131,
+              "dy": 21,
+              "rotate": true
+            }
+          },
+          "priority": true
+        },
+        "router": {
+          "name": "orthogonal",
+          "args": {
+            "padding": 8
+          }
+        },
+        "connector": {
+          "name": "rounded"
+        },
+        "labels": [
+          {
+            "attrs": {
+              "text": {
+                "text": "EV_SYS_XX_BTN_UP"
+              }
+            },
+            "position": {
+              "distance": 0.5041586742264837,
+              "offset": 68,
+              "angle": 0
+            }
+          },
+          {
+            "attrs": {
+              "label": {
+                "text": "2",
+                "fill": "#555555"
+              },
+              "circleBody": {
+                "fill": "#F0F0F0",
+                "stroke": "#555555"
+              }
+            }
+          },
+          {
+            "attrs": {}
+          },
+          {
+            "attrs": {}
+          }
+        ],
+        "id": "8e598810-1d2d-4cc1-8a22-68b4f8d3c937",
+        "z": 31,
+        "parent": "db372094-acc6-467b-b44d-495606208445",
+        "vertices": [
+          {
+            "x": 684,
+            "y": 221
+          }
+        ],
+        "attrs": {}
+      }
+    ]
+  },
+  "genModel": {
+    "generator": {
+      "type": "create::c",
+      "features": {
+        "Outlet": {
+          "targetProject": "",
+          "targetFolder": "",
+          "libraryTargetFolder": "",
+          "skipLibraryFiles": "",
+          "apiTargetFolder": ""
+        },
+        "LicenseHeader": {
+          "licenseText": ""
+        },
+        "FunctionInlining": {
+          "inlineReactions": false,
+          "inlineEntryActions": false,
+          "inlineExitActions": false,
+          "inlineEnterSequences": false,
+          "inlineExitSequences": false,
+          "inlineChoices": false,
+          "inlineEnterRegion": false,
+          "inlineExitRegion": false,
+          "inlineEntries": false
+        },
+        "OutEventAPI": {
+          "observables": false,
+          "getters": false
+        },
+        "IdentifierSettings": {
+          "moduleName": "MyFirstStatechart",
+          "statemachinePrefix": "myFirstStatechart",
+          "separator": "_",
+          "headerFilenameExtension": "h",
+          "sourceFilenameExtension": "c"
+        },
+        "Tracing": {
+          "enterState": "",
+          "exitState": "",
+          "generic": ""
+        },
+        "Includes": {
+          "useRelativePaths": false,
+          "generateAllSpecifiedIncludes": false
+        },
+        "GeneratorOptions": {
+          "userAllocatedQueue": false,
+          "metaSource": false
+        },
+        "GeneralFeatures": {
+          "timerService": false,
+          "timerServiceTimeType": ""
+        },
+        "Debug": {
+          "dumpSexec": false
+        }
+      }
+    }
+  }
+}
